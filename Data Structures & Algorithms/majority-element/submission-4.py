dfe9b999af7513833d29f79afc509f059cb1846a
Sarveshvaran Senthilkumar <sarveshvaranemail@gmail.com>
2026-08-19 12:19:01 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Boyer–Moore is to track candidate and count
+        ans=-1
+        count=0
+
+        for i in nums:
+            if count==0:
+                ans=i
+
+            if ans==i:
+                count+=1
+            else:
+                count-=1
+
+        return ans 
+            
